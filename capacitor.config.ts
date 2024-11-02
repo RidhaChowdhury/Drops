7fe5d3c3f32ccebdb089f08a7ffb7ae6237bb018
Ridha Chowdhury <chowdhuryridha@gmail.com>
2024-11-02 18:54:55 -0400

    Splash screen and drippy icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,20 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.ridhachowdhury.hydrated',
-  appName: 'hydrated',
-  webDir: 'dist'
+   appId: 'com.ridhachowdhury.hydrated',
+   appName: 'hydrated',
+   webDir: 'dist',
+   plugins: {
+      SplashScreen: {
+         launchShowDuration: 3000, // Duration in milliseconds
+         launchAutoHide: true, // Automatically hide splash screen after duration
+         backgroundColor: '#0A0A0F', // Splash screen background color (HEX format)
+         showSpinner: true, // Show spinner on splash screen
+         androidSpinnerStyle: 'large', // Spinner style for Android
+         iosSpinnerStyle: 'small', // Spinner style for iOS
+         spinnerColor: '#FFFFFF', // Spinner color
+      },
+   },
 };
 
 export default config;
--- src/screens/Metrics.tsx
+++ src/screens/Metrics.tsx
@@ -1,10 +1,7 @@
-import { useSettings } from '@/hooks/useSettings';
 import { useTheme } from '@/hooks/theme-provider';
 
-import { convertFromOunces, convertToOunces } from '@/utils/conversionUtils';
-
 export default function MetricsScreen() {
-   const { theme, setTheme } = useTheme();
+   const { theme } = useTheme();
    return (
       <div
          className={`relative flex flex-col items-center justify-center h-screen font-sans ${theme === 'dark' ? 'bg-gray-900 text-white' : 'bg-gray-100 text-black'}`}
