4423bce18500925b86c7913390c96b2bdeffa60f
Ridha Chowdhury <chowdhuryridha@gmail.com>
2024-11-02 19:19:47 -0400

    splash and icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,13 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-   appId: 'com.ridhachowdhury.hydrated',
-   appName: 'hydrated',
+   appId: 'com.ridhachowdhury.Drops',
+   appName: 'Drops',
    webDir: 'dist',
    plugins: {
       SplashScreen: {
          launchShowDuration: 3000, // Duration in milliseconds
+         launchFadeOutDuration: 3000, // Duration in milliseconds
          launchAutoHide: true, // Automatically hide splash screen after duration
          backgroundColor: '#0A0A0F', // Splash screen background color (HEX format)
          showSpinner: true, // Show spinner on splash screen
